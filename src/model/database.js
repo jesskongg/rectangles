399bb98c2c98cc946409484030058c52cba77dce
Jessica Kong <jka109@sfu.ca>
2020-11-25 02:03:01 -0800

    Remove use of env variables

--- src/model/database.js
+++ src/model/database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 var db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: 'asn3-4-db',
+  user: 'CLIENT',
+  password: 'client_CMPT470',
+  database: 'cmpt470'
 });
 db.connect();
 
