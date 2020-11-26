da4a65a50d7654226eb7684e34b98e230cbfbc2d
Jessica Kong <jka109@sfu.ca>
2020-11-25 20:18:18 -0800

    Modify header

--- src/model/middlewares/setHeaders.js
+++ src/model/middlewares/setHeaders.js
@@ -3,7 +3,7 @@ const setHeaders = ((req, res, next) => {
   res.set("Pragma", "no-cache");
   res.set("Cache-Control", "no-cache, no-store, must-revalidate");
   res.set("X-XSS-Protection", "1; mode=block");
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "http://localhost:8080");
   res.set("Access-Control-Allow-Headers", "access-control-reques-headers");  // Yes, there is a typo somewhere in the front end
   next();
 });
