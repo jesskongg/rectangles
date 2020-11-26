a233ac40efc7e50fc102c789bbb24b30c01e3f58
Jessica Kong <jka109@sfu.ca>
2020-11-25 22:31:57 -0800

    Update files

--- src/model/index.js
+++ src/model/index.js
@@ -12,18 +12,28 @@ router.get('/', function(req, res, next) {
 });
 
 // Change attributes of any of the rectangles
-router.put('/', function(req, res, next) {
-  // model query to change specific attribute
+router.put('/update', function(req, res, next) {
+  putRectangles(req.body.columnName, req.body.value, req.body.id);
 });
 
 // Delete existing rectangles -- PUT?
 router.put('/delete', function(req, res, next) {
-
+  deleteRectangles(id)
+  res.send(200);
 });
 
 // Add new rectangles
-router.post('/', function(req, res, next) {
-  // model query to add entry to rectangle table
+router.post('/add', function(req, res, next) {
+  postRectangles(
+    req.body.id,
+    req.body.width,
+    req.body.height,
+    req.body.color,
+    req.body.radius,
+    req.body.opacity,
+    req.body.cursorType
+  );
+  res.send(200);
 });
 
 export default router;
